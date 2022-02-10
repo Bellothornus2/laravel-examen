aae574ab2cecece99d5405bf4ebf81ab6df61970
bellothornus2 <divanov@cifpfbmoll.eu>
2022-02-10 16:03:23 +0100

    feat: :sparkles: migracion hecha

---
+++ laravel-examen/database/migrations/2022_02_10_145350_create_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->boolean("caducable");
+            $table->boolean("comentable");
+            $table->string("acceso");
+            $table->string("id_usuario");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+};
